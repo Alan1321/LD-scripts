d0c087d0d2e05a38e53c27af45e4871165f1d181
alansubedi123 <alansubedi123@gmail.com>
2024-01-25 11:29:21 -0600

    script produces both nalma and glm COGS

--- workflow_for_NALMA_GLM/glm.py
+++ workflow_for_NALMA_GLM/glm.py
@@ -1,12 +1,76 @@
 import math
+import numpy as np
+import xarray as xa
+from input_output import output_cog
 
 class preprocess_glm():
     def __init__(self, file, variable, location):
         self.file = file
+        self.variable = variable
+        self.location = location
         self.r_eq, self.r_pol, self.H, self.lon0 = self.extract_variables()
         
     def connector(self):
-        pass
+        lat, lon, data = self.copy_lat_lon_data()
+        file = self.generate_new_xarray(lat, lon, data)
+        self.generate_cog(file)
+    
+    def generate_cog(self, file):
+        print("generating cog ...")
+        file = file.transpose('lat', 'lon')
+        file.rio.set_spatial_dims(x_dim='lon', y_dim='lat', inplace=True)
+        file.rio.crs
+        file.rio.set_crs('epsg:4326', inplace=True)
+
+        directory_name, filename = self.extract_file_directory_name(self.location)
+        output_cog(file, directory_name, filename)
+
+    def extract_file_directory_name(self, location):
+        filename = location.split('/')[-1]
+        filename = filename[:-3]
+        directory_name = "GLM"
+
+        return directory_name, filename
+
+    def copy_lat_lon_data(self):
+        print('copying lat, lon, data ...')
+        x_cpy = self.file.x.copy()
+        y_cpy = self.file.y.copy()
+
+        lat = np.zeros(self.file.x.size)
+        lon = np.zeros(self.file.x.size)
+        data = self.file[self.variable].data.copy()
+
+        for i in range(x_cpy.size):
+            lat[i], lon[i] = self.calc_lat_lon(x_cpy[i], y_cpy[i])
+
+        _lat = lat.copy()
+        _lon = lon.copy()
+        _data = self.file[self.variable].data.copy()
+
+        end_index = len(lon)
+        for index, value in np.ndenumerate(lon[::-1]):
+            end_index -= 1
+            if value == -999.0:
+                _data = np.delete(_data, end_index, axis=0)
+                _lon = np.delete(_lon, end_index)
+
+        end_index = len(lat)
+        for index, value in np.ndenumerate(lat[::-1]):
+            end_index -= 1
+            if value == -999.0:
+                _data = np.delete(_data, end_index, axis=1)
+                _lat = np.delete(_lat, end_index)
+
+        return _lat, _lon, _data
+    
+    def generate_new_xarray(self, lat, lon, data):
+        file = xa.Dataset(
+            data_vars={"Flash_extent_density": (("lon", "lat"), data)},
+            coords={"lon": lon, "lat": lat},
+            attrs={"instrument_ID": "GLM-1"}
+        )
+        return file
 
     def extract_variables(self):
         #radius of Earth at the equator in meters
@@ -19,11 +83,11 @@ class preprocess_glm():
         lon0 = self.file['goes_imager_projection'].longitude_of_projection_origin * (math.pi/180.)
         return r_eq, r_pol, H, lon0
 
-    def calc_lat_lon(xx,yy):
+    def calc_lat_lon(self, xx,yy):
         #intermediate calculations
-        a = (math.sin(xx))**2 + (math.cos(xx))**2 * ((math.cos(yy))**2 + r_eq**2/r_pol**2*(math.sin(yy))**2)
-        b = -2 * H * math.cos(xx) * math.cos(yy)
-        c = H**2 - r_eq**2
+        a = (math.sin(xx))**2 + (math.cos(xx))**2 * ((math.cos(yy))**2 + self.r_eq**2/self.r_pol**2*(math.sin(yy))**2)
+        b = -2 * self.H * math.cos(xx) * math.cos(yy)
+        c = self.H**2 - self.r_eq**2
 
         if b**2-4*a*c < 0:
             lat = -999.
@@ -39,8 +103,8 @@ class preprocess_glm():
 
             #geodetic latitude and longitude for point P
             #math.atan(x) return the arc tangent of x, in radians. The result is between -pi/2 and pi/2
-            lat = math.atan(((r_eq**2)/(r_pol**2)) * (s_z/math.sqrt((H-s_x)**2 + s_y**2))) #radians
-            lon = lon0 - math.atan(s_y/(H-s_x)) #radians
+            lat = math.atan(((self.r_eq**2)/(self.r_pol**2)) * (s_z/math.sqrt((self.H-s_x)**2 + s_y**2))) #radians
+            lon = self.lon0 - math.atan(s_y/(self.H-s_x)) #radians
 
             #convert radians to degrees
             lat = lat * 180./math.pi #degree
--- workflow_for_NALMA_GLM/main.py
+++ workflow_for_NALMA_GLM/main.py
@@ -1,7 +1,7 @@
 from input_output import read_nc_file, read_local_nc_file_glm
 from nalma import preprocess_nalma
 from utils import get_all_files_in_a_directory
-
+from glm import preprocess_glm
 
 # #NALMA
 # all_local_nalma_files = get_all_files_in_a_directory("/home/asubedi/test_cog/")
@@ -15,4 +15,4 @@ from utils import get_all_files_in_a_directory
 location = "/home/asubedi/Desktop/data/raw-files/GLM/OR_GLM-L3-GLMF-M6_G16_s202315823490000_e202315823500000_c20231582351080.nc"
 file = read_local_nc_file_glm(location)
 variable = 'Flash_extent_density'
-print(file)
+preprocess_glm(file, variable, location).connector()
