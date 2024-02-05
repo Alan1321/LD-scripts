59c0b33900dd1eb6b23737b7b7e048167ba52a41
alansubedi123 <alansubedi123@gmail.com>
2024-02-05 12:01:45 -0600

    remove datasets or nc files after geotiff conversion is complete

--- workflow_for_NALMA_GLM/main.py
+++ workflow_for_NALMA_GLM/main.py
@@ -2,9 +2,10 @@ from input_output import read_nc_file
 from nalma import preprocess_nalma
 from utils import get_all_nalma_files_in_a_directory, get_all_files_in_a_directory
 from glm import preprocess_glm
+import shutil
 
-BASE_PATH = '/home/asubedi/'
 BASE_PATH = '/home/ubuntu/'
+BASE_PATH = '/home/asubedi/'
 
 #NALMA
 all_local_nalma_files = get_all_nalma_files_in_a_directory(f"{BASE_PATH}data/NALMA_input/")
@@ -22,3 +23,7 @@ for each_file_location in all_local_glm_files:
     file = read_nc_file(each_file_location)
     variable = 'Flash_extent_density'
     preprocess_glm(file, variable, each_file_location).connector()
+
+
+shutil.rmtree(f"{BASE_PATH}data/NALMA_input/")
+shutil.rmtree(f"{BASE_PATH}data/GLM_input/")
