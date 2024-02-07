0ea9c4a6607a22a946f182b971ac31b9774a443e
alansubedi123 <alansubedi123@gmail.com>
2024-02-07 12:29:30 -0600

    complete, need to add s3 storage script now

--- automate_nalma_glm/main.py
+++ automate_nalma_glm/main.py
@@ -9,6 +9,21 @@ from workflow_for_NALMA_GLM.main import convert_nalma_glm
 from lmatools_outer.examples.test import generate_nc_for_nalma
 from const import BASE_PATH
 
-# download_nalma_or_glm("nalma", '2020-03-05','2020-03-06', BASE_PATH)
-# generate_nc_for_nalma(BASE_PATH)
-convert_nalma_glm(BASE_PATH)
+
+def start(dataset_name, date_start, date_end):
+    download_nalma_or_glm(dataset_name, date_start, date_end, BASE_PATH)
+    if dataset_name == 'nalma':
+        generate_nc_for_nalma(BASE_PATH)
+    convert_nalma_glm(BASE_PATH)
+
+#sample nalma test
+dataset_name = 'nalma'
+date_start, date_end = '2020-03-05','2020-03-06'
+
+#sample glm test
+dataset_name = 'glm'
+date_start = "2024-01-01 00:00:00"
+date_end = "2024-01-01 01:59:59"
+
+#start processing
+start(dataset_name, date_start, date_end)
